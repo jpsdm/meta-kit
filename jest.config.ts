ca1be3b6197b8a54c0ef9f0b26d8e17bb14c1f0f
João Pedro Soares <dev.jpsdm@gmail.com>
2025-05-04 21:35:53 -0300

    test: add coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,15 @@ import type { Config } from 'jest';
 
 const config: Config = {
   projects: ['<rootDir>/packages/whatsapp'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  collectCoverage: true,
 };
 
 export default config;
